0d30b363d657d13ea725d24cacebe0b2f73db0b3
philkuz <superpker@gmail.com>
2017-02-10 15:27:40 -0800

    Add new moc list

--- server/imports/moc.js
+++ server/imports/moc.js
@@ -1,5 +1,11 @@
 
 var mocTaxonomy = {
+    "question": {
+        "Context based question": "Did you ever find the dog you were looking for?",
+        "Ask for Opinion": "What do you think about 80s music?",
+        "Clarifying Question": "What do you mean you jumped over the grand canyon?",
+        "Factoid Question": "What's the capital of Alabama?"
+    },
     "conversational_formality": {
         "closing": "Goodbye",
         "courtesies": [
@@ -14,39 +20,52 @@ var mocTaxonomy = {
         "compliment": "You sound like you're doing so well"
     },
     "declaration": {
-        "offer": "I can inform the president, if you want.",
         "opinionated": "I think vegetarian parents should give children the choice to be vegetarian or not.",
-        "non opinionated": "Some of Hemingway's cats had 6 toes on each paw.",
+        "fact / certain declaration" : "The capital of Alabama is Montegomery.",
+        "uncertain declaration" : "I think Hemingway's cats might have had 6 toes on each paw.",
+        "Some of Hemingway's cats had 6 toes on each paw.",
         "exclamation": "Wow.",
-        "future-commitment": "Let me tell my wife about this chat."
+        "offer": "I can inform the president, if you want.",
+        "future-commitment": "I'm gonna tell my wife about this chat.",
+    },
+    "narrative" : {
+        "start story" : "One time I was spelunking in Santa Cruz",
+        "narrative content" : "While I was there, I found a spider the size of my hand",
     },
     "other": {
-        "pause": "Um."
+        "filler word": "Um."
     },
     "influence": {
-        "Rhetorical-Questions": "Can you blame your dog for wanting attention?",
+        "Rhetorical-Question": "Can you blame your dog for wanting attention?",
         "steer conversation": "I'm gonna change the subject to Baseball",
         "suggestion": "Don't you think you should listen to what your kids say?",
-        "command": "Stop telling your dog rude things!"
+        "command": "Stop telling your dog to jump through hoops!"
     },
     "understanding": {
         "appreciation": "Thanks so much for the advice",
+        "apology": "My bad.",
         "acknowledgement": "I know what you're talking about ",
-        "apology": "My bad."
+        "full disagreement" : "The world doesn't work that way.",
+        "partial disagreement": "I don't think you're right, but I'm not sure",
+        "partial agreement": "I guess you're right, but I think it's possible they may have thought of this before",
+        "full agreement": "Absolutely true.",
+
     },
     "identity": {
         "identifying information": [
             "I am thirty years old",
-            "I was born in a factor"
-        ]
+            "I was born in a factory"
+        ],
+        "referene to past" : "I once backed my car into a trash can.",
     },
     "answer": {
         "factoid based answer": "The capital of Alabama is Montegomery.",
-        "i dont know answer": "I couldn't tell you whether or not that's true",
-        "maybe": "Maybe",
-        "opinion based answer": "We should give free coffee to tired people.",
+        "opinion based answer": "I think we should give free coffee to tired people.",
+        "i dont know answer": "I'm not really not sure",
+        "maybe": "That may be the case, but I'm not sure",
+
         "full disagreement": "Nope",
-        "partial disagreement": "I don't think you're right, but I don't know much about this situation",
+        "partial disagreement": "I don't think you're right, but I'm not sure",
         "partial agreement": "I guess you're right, but I think it's possible they may have thought of this before",
         "full agreement": "Absolutely true.",
         "other answer": ""
@@ -58,20 +77,16 @@ var mocTaxonomy = {
         "Signal non-understanding": "Um, I'm not sure what you mean by that.",
         "Repeat phrase": "I said, I'll teach my dog how to jump through the hoop."
     },
-    "question": {
-        "Context based question": "Did you ever find the dog you were looking for?",
-        "Opinion based question": "What do you think about 80s music?",
-        "clarifying question": "What do you mean you jumped over the grand canyon?",
-        "Factoid question": "What's the capital of Alabama?"
-    },
+
     "humor": {
-        "Rhetorical-Questions": "Can you blame your dog for wanting attention?",
+        "rhetorical question": "Can you blame your dog for wanting attention?",
         "wit/wordplay": "Time flies like an arrow; fruit flies like a banana.",
         "pun": "I'm reading a book about anti-gravity. It's impossible to put down.",
         "joke": "I dreamt I was forced to eat a giant marshmallow. When I woke up, my pillow was gone.",
         "sarcasm": "I totally want to be run over by a car"
     }
 }
+
 
 
 export { mocTaxonomy };
